91f747943aa7df71aefa57317929ccb2e051308f
Joel Ostapowich <JOstapowich@tcf.com>
2019-01-20 23:48:55 -0600

    changed flow so that results (and errors) are accumulated without having to start fresh for each display point

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ namespace MessingWithNumbers
         const decimal MULT_DEC = (decimal)MULT_F;
 
         static uint result_uint;
-        static long result_long;
+        static ulong result_long;
         static float result_float;
         static double result_double;
         static decimal result_decimal;
@@ -77,136 +77,117 @@ namespace MessingWithNumbers
 
             long t_uint, t_long, t_float, t_double, t_decimal;
 
-            t_uint = Loop_Int(iterations, out result_uint);
+            t_uint = Loop_Int(iterations);
 
-            t_long = Loop_Long(iterations, out result_long);
+            t_long = Loop_Long(iterations);
 
-            t_float = Loop_Float(iterations, out result_float);
+            t_float = Loop_Float(iterations);
 
-            t_double = Loop_Double(iterations, out result_double);
+            t_double = Loop_Double(iterations);
 
-            t_decimal = Loop_Decimal(iterations, out result_decimal);
+            t_decimal = Loop_Decimal(iterations);
 
-            decimal baseline = (decimal)(iterations * MULT_F);
+            decimal baseline = (decimal)iterations * (decimal)MULT_F;
 
-            Console.WriteLine("{0,10}{1,30}{2,30}{3,20}", "Test", "Result", "Delta", "Time");
-            Console.WriteLine(new string('-', 95));
+            Console.WriteLine("{0,10}{1,30}{2,30}{3,20}{4,20}", "Test", "Result", "Delta", "Time", "X Faster");
+            Console.WriteLine(new string('-', 115));
             Console.WriteLine("{0,10}{1,30:N10}", "Baseline", baseline);
-            Console.WriteLine("{0,10}{1,30:N10}{2,30:N10}{3,20:N0}", "Float", result_float, baseline - (decimal)result_float, t_float);
-            Console.WriteLine("{0,10}{1,30:N10}{2,30:N10}{3,20:N0}", "Double", result_double, baseline - (decimal)result_double, t_double);
-            Console.WriteLine("{0,10}{1,30:N10}{2,30:N10}{3,20:N0}", "Decimal", result_decimal, baseline - result_decimal, t_decimal);
-            Console.WriteLine("{0,10}{1,30:N10}{2,30}{3,20:N0}", "Int", result_uint, "", t_uint);
-            Console.WriteLine("{0,10}{1,30:N10}{2,30}{3,20:N0}", "Long", result_long, "", t_long);
+            Console.WriteLine("{0,10}{1,30:N10}{2,30:N10}{3,20:N0}{4,20:0.00}", "Float", result_float, result_decimal - (decimal)result_float, t_float, (double)t_decimal / (double)t_float  );
+            Console.WriteLine("{0,10}{1,30:N10}{2,30:N10}{3,20:N0}{4,20:0.00}", "Double", result_double, result_decimal - (decimal)result_double, t_double, (double)t_decimal / (double)t_double);
+            Console.WriteLine("{0,10}{1,30:N10}{2,30:N10}{3,20:N0}{4,20:0.00}", "Decimal", result_decimal, result_decimal - result_decimal, t_decimal, (double)t_decimal / (double)t_decimal);
+            Console.WriteLine("{0,10}{1,30:N10}{2,30}{3,20:N0}{4,20:0.00}", "Int", result_uint, "", t_uint, (double)t_long / (double)t_uint);
+            Console.WriteLine("{0,10}{1,30:N10}{2,30}{3,20:N0}{4,20:0.00}", "Long", result_long, "", t_long, (double)t_long / (double)t_long);
 
 
             _time_inmath = _time_inmath + t_float + t_double + t_decimal + t_uint + t_long;
         }
 
-        private static long Loop_Int(ulong iterations, out uint result)
+        private static long Loop_Int(ulong iterations)
         {
             Stopwatch sw = new Stopwatch();
-
-            uint x = 0;
 
             sw.Reset();
             sw.Start();
 
             for (ulong i = 0; i < iterations; i++)
             {
-                x = x + 1 * 2;
+                result_uint = result_uint + 1 * 2;
             }
 
             sw.Stop();
 
-            result = x;
 
             return sw.ElapsedTicks;
         }
 
-        private static long Loop_Long(ulong iterations, out long result)
+        private static long Loop_Long(ulong iterations)
         {
             Stopwatch sw = new Stopwatch();
-
-            long x = 0;
 
             sw.Reset();
             sw.Start();
 
             for (ulong i = 0; i < iterations; i++)
             {
-                x = x + 1 * 2;
+                result_long = result_long + 1 * 2;
             }
 
             sw.Stop();
-
-            result = x;
 
             return sw.ElapsedTicks;
         }
 
         //[MethodImpl(MethodImplOptions.NoOptimization)]  //comment (all three) for lesson on .NET compiler optimizations (or compare DEBUG run vs RELEASE)
-        private static long Loop_Float(ulong iterations, out float result)
+        private static long Loop_Float(ulong iterations)
         {
             Stopwatch sw = new Stopwatch();
-
-            float x = 0.25F;
 
             sw.Reset();
             sw.Start();
 
             for (ulong i = 0; i < iterations; i++)
             {
-                x = x + 1 * MULT_F;
+                result_float = result_float + 1 * MULT_F;
             }
 
             sw.Stop();
-
-            result = x;
 
             return sw.ElapsedTicks;
         }
 
         //[MethodImpl(MethodImplOptions.NoOptimization)]
-        private static long Loop_Double(ulong iterations, out double result)
+        private static long Loop_Double(ulong iterations)
         {
             Stopwatch sw = new Stopwatch();
-
-            double x = 0.25D;
 
             sw.Reset();
             sw.Start();
 
             for (ulong i = 0; i < iterations; i++)
             {
-                x = x + 1 * MULT_D;
+                result_double = result_double + 1 * MULT_D;
             }
 
             sw.Stop();
-
-            result = x;
 
             return sw.ElapsedTicks;
         }
 
         //[MethodImpl(MethodImplOptions.NoOptimization)]
-        private static long Loop_Decimal(ulong iterations, out decimal result)
+        private static long Loop_Decimal(ulong iterations)
         {
             Stopwatch sw = new Stopwatch();
-
-            decimal x = 0.25M;
 
             sw.Reset();
             sw.Start();
             
             for (ulong i = 0; i < iterations; i++)
             {
-                x = x + 1 * MULT_DEC;
+                result_decimal = result_decimal + 1 * MULT_DEC;
             }
 
             sw.Stop();
 
-            result = x;
-            
             return sw.ElapsedTicks;
         }
 
